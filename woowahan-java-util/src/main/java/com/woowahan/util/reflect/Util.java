51c035b1972e24b3cc29161688f256b33433fc93
AnJaeSeongS2 <jindam91@naver.com>
2021-04-06 19:36:23 +0900

    [trivial][java-util] reflection 기능 추가, 및 accesible 변경에 대한 경고 추가.

--- woowahan-java-util/src/main/java/com/woowahan/util/reflect/Util.java
+++ woowahan-java-util/src/main/java/com/woowahan/util/reflect/Util.java
@@ -11,49 +11,51 @@ import java.lang.reflect.Method;
  */
 public class Util {
 
-    public static void setField(Object settableObject, String fieldName, Object fieldObject) throws NoSuchFieldException, IllegalAccessException {
-        settableObject.getClass().getDeclaredField(fieldName).set(settableObject, fieldObject);
+    public static Object invokeStaticMethod(Class<?> target, String methodName, Class<?>[] methodParamTypes, Object... argsForInvoke) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return target.getMethod(methodName, methodParamTypes).invoke(null, argsForInvoke);
     }
 
-    public static Object getField(Object gettableObject, String fieldName) throws NoSuchFieldException, IllegalAccessException {
-        return gettableObject.getClass().getDeclaredField(fieldName).get(gettableObject);
+    public static Object invokeStaticMethod(Class<?> target, String methodName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return invokeStaticMethod(target, methodName, new Class[0], new Object[0]);
     }
 
-    public static Object invokeMethod(Object invokableObject, String methodName, Class<?>[] methodParamTypes, Object... argsForInvoke) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    public static Object invokeMethod(Object target, String methodName, Class<?>[] methodParamTypes, Object... argsForInvoke) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         Method method;
-        if (invokableObject instanceof Annotation) {
-            method = ((Annotation) invokableObject).annotationType().getDeclaredMethod(methodName, methodParamTypes);
+        if (target instanceof Annotation) {
+            method = ((Annotation) target).annotationType().getMethod(methodName, methodParamTypes);
         } else {
-            method = invokableObject.getClass().getDeclaredMethod(methodName, methodParamTypes);
+            method = target.getClass().getMethod(methodName, methodParamTypes);
         }
-        return method.invoke(invokableObject, argsForInvoke);
+        return method.invoke(target, argsForInvoke);
     }
 
-    public static Object invokeMethod(Object invokableObject, String methodName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Method method;
-        if (invokableObject instanceof Annotation) {
-            method = ((Annotation) invokableObject).annotationType().getDeclaredMethod(methodName);
-        } else {
-            method = invokableObject.getClass().getDeclaredMethod(methodName);
-        }
-        return method.invoke(invokableObject);
+    public static Object invokeMethod(Object target, String methodName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return invokeMethod(target, methodName, new Class[0], new Object[0]);
+    }
+
+    public static void setField(Object settableTarget, String fieldName, Object fieldObject) throws NoSuchFieldException, IllegalAccessException {
+        settableTarget.getClass().getField(fieldName).set(settableTarget, fieldObject);
+    }
+
+    public static Object getField(Object gettableTarget, String fieldName) throws NoSuchFieldException, IllegalAccessException {
+        return gettableTarget.getClass().getField(fieldName).get(gettableTarget);
     }
 
     /**
      * weakness to multithread programming.
      * so, jdk 1.9 deprecated about setAccessible(boolean).
      * but, this method exists for Test logics.
-     * @param settableObject
+     * @param target
      * @param fieldName
      * @param fieldObject
      * @throws NoSuchFieldException
      * @throws IllegalAccessException
      */
-    public static synchronized void setFieldAnyway(Object settableObject, String fieldName, Object fieldObject) throws NoSuchFieldException, IllegalAccessException {
-        Field field = settableObject.getClass().getDeclaredField(fieldName);
+    public static synchronized void setFieldAnyway(Object target, String fieldName, Object fieldObject) throws NoSuchFieldException, IllegalAccessException {
+        Field field = findFieldAnyway(target.getClass(), fieldName);
         boolean savedAccessible = field.isAccessible();
         field.setAccessible(true);
-        field.set(settableObject, fieldObject);
+        field.set(target, fieldObject);
         field.setAccessible(savedAccessible);
     }
 
@@ -61,17 +63,17 @@ public class Util {
      * weakness to multithread programming.
      * so, jdk 1.9 deprecated about setAccessible(boolean).
      * but, this method exists for Test logics.
-     * @param gettableObject
+     * @param target
      * @param fieldName
      * @return
      * @throws NoSuchFieldException
      * @throws IllegalAccessException
      */
-    public static synchronized Object getFieldAnyway(Object gettableObject, String fieldName) throws NoSuchFieldException, IllegalAccessException {
-        Field field = gettableObject.getClass().getDeclaredField(fieldName);
+    public static synchronized Object getFieldAnyway(Object target, String fieldName) throws NoSuchFieldException, IllegalAccessException {
+        Field field = findFieldAnyway(target.getClass(), fieldName);
         boolean savedAccessible = field.isAccessible();
         field.setAccessible(true);
-        Object result = field.get(gettableObject);
+        Object result = field.get(target);
         field.setAccessible(savedAccessible);
         return result;
     }
@@ -80,7 +82,22 @@ public class Util {
      * weakness to multithread programming.
      * so, jdk 1.9 deprecated about setAccessible(boolean).
      * but, this method exists for Test logics.
-     * @param invokableObject
+     * @param target
+     * @param methodName
+     * @return
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
+    public static synchronized Object invokeMethodAnyway(Object target, String methodName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return invokeMethodAnyway(target, methodName, new Class[0], new Object[0]);
+    }
+
+    /**
+     * weakness to multithread programming.
+     * so, jdk 1.9 deprecated about setAccessible(boolean).
+     * but, this method exists for Test logics.
+     * @param target
      * @param methodName
      * @param methodParamTypes
      * @param argsForInvoke
@@ -89,16 +106,11 @@ public class Util {
      * @throws InvocationTargetException
      * @throws IllegalAccessException
      */
-    public static synchronized Object invokeMethodAnyway(Object invokableObject, String methodName, Class<?>[] methodParamTypes, Object... argsForInvoke) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Method method;
-        if (invokableObject instanceof Annotation) {
-            method = ((Annotation) invokableObject).annotationType().getDeclaredMethod(methodName, methodParamTypes);
-        } else {
-            method = invokableObject.getClass().getDeclaredMethod(methodName, methodParamTypes);
-        }
+    public static synchronized Object invokeMethodAnyway(Object target, String methodName, Class<?>[] methodParamTypes, Object... argsForInvoke) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = findMethodAnyway(target.getClass(), methodName, methodParamTypes);
         boolean savedAccessible = method.isAccessible();
         method.setAccessible(true);
-        Object result = method.invoke(invokableObject, argsForInvoke);
+        Object result = method.invoke(target, argsForInvoke);
         method.setAccessible(savedAccessible);
         return result;
     }
@@ -107,24 +119,82 @@ public class Util {
      * weakness to multithread programming.
      * so, jdk 1.9 deprecated about setAccessible(boolean).
      * but, this method exists for Test logics.
-     * @param invokableObject
+     * @param target
      * @param methodName
      * @return
      * @throws NoSuchMethodException
      * @throws InvocationTargetException
      * @throws IllegalAccessException
      */
-    public static synchronized Object invokeMethodAnyway(Object invokableObject, String methodName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Method method;
-        if (invokableObject instanceof Annotation) {
-            method = ((Annotation) invokableObject).annotationType().getDeclaredMethod(methodName);
-        } else {
-            method = invokableObject.getClass().getDeclaredMethod(methodName);
-        }
+    public static synchronized Object invokeStaticMethodAnyway(Class<?> target, String methodName) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return invokeStaticMethodAnyway(target, methodName, new Class[0], new Object[0]);
+    }
+
+    /**
+     * weakness to multithread programming.
+     * so, jdk 1.9 deprecated about setAccessible(boolean).
+     * but, this method exists for Test logics.
+     * @param target
+     * @param methodName
+     * @param methodParamTypes
+     * @param argsForInvoke
+     * @return
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
+    public static synchronized Object invokeStaticMethodAnyway(Class<?> target, String methodName, Class<?>[] methodParamTypes, Object... argsForInvoke) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = findMethodAnyway(target, methodName, methodParamTypes);
         boolean savedAccessible = method.isAccessible();
         method.setAccessible(true);
-        Object result = method.invoke(invokableObject);
+        Object result = method.invoke(null, argsForInvoke);
         method.setAccessible(savedAccessible);
         return result;
+    }
+
+    public static Method findMethodAnyway(Class<?> target, String methodName, Class<?>[] methodParamTypes) throws NoSuchMethodException {
+        if (target == null) {
+            throw new NoSuchMethodException("methodName : " + methodName + " methodParamTypes : " + methodParamTypes.toString());
+        }
+        Method method;
+        try {
+            method = target.getMethod(methodName, methodParamTypes);
+        } catch (NoSuchMethodException e) {
+            method = findMethodWithUnAccessible(target, methodName, methodParamTypes);
+        }
+        return method;
+    }
+
+    public static Field findFieldAnyway(Class<?> target, String fieldName) throws NoSuchFieldException {
+        if (target == null) {
+            throw new NoSuchFieldException("fieldName : " + fieldName);
+        }
+        Field field;
+        try {
+            field = target.getField(fieldName);
+        } catch (NoSuchFieldException e) {
+            field = findFieldWithUnAccessible(target, fieldName);
+        }
+        return field;
+    }
+
+    private static Method findMethodWithUnAccessible(Class<?> target, String methodName, Class<?>[] methodParamTypes) throws NoSuchMethodException {
+        try {
+            return target.getDeclaredMethod(methodName, methodParamTypes);
+        } catch (NoSuchMethodException e) {
+            if (target.getSuperclass() == null)
+                throw e;
+            return findMethodWithUnAccessible(target.getSuperclass(), methodName, methodParamTypes);
+        }
+    }
+
+    private static Field findFieldWithUnAccessible(Class<?> target, String fieldName) throws NoSuchFieldException {
+        try {
+            return target.getDeclaredField(fieldName);
+        } catch (NoSuchFieldException e) {
+            if (target.getSuperclass() == null)
+                throw e;
+            return findFieldWithUnAccessible(target.getSuperclass(), fieldName);
+        }
     }
 }
--- woowahan-java-util/src/test/java/com/woowahan/util/reflect/UtilTest.java
+++ woowahan-java-util/src/test/java/com/woowahan/util/reflect/UtilTest.java
@@ -15,31 +15,177 @@ import static org.junit.jupiter.api.Assertions.*;
 class UtilTest {
 
     @Test
-    void invokeMethodAnyway() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+    public void invokeMethod() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Parent parent = new Parent();
+        Child child = new Child();
+        assertEquals("a", Util.invokeMethod(parent, "getA"));
+        assertThrows(NoSuchMethodException.class, () -> Util.invokeMethod(parent, "getC"));
+        assertEquals("a", Util.invokeMethod(child, "getA"));
+        assertEquals("c", Util.invokeMethod(child, "getC"));
+
+        assertThrows(NoSuchMethodException.class, () -> Util.invokeMethod(parent, "getB"));
+        assertThrows(NoSuchMethodException.class, () -> Util.invokeMethod(child, "getB"));
+    }
+
+    @Test
+    public void invokeMethodAnyway() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         TestClass obj = new TestClass();
         assertEquals("successMethod", Util.invokeMethodAnyway(obj, "method"));
         assertThrows(NoSuchMethodException.class, () -> Util.invokeMethodAnyway(obj, "method", new Class<?>[]{String.class, String.class}, "a", "b"));
         assertEquals("successMethodab", Util.invokeMethodAnyway(obj, "methodManyParams", new Class<?>[]{String.class, String.class}, "a", "b"));
         assertEquals("sucessMethodOnAnnotation", Util.invokeMethodAnyway(AnnotationOnClass.class.getDeclaredAnnotations()[0], "method"));
+
+        Parent parent = new Parent();
+        Child child = new Child();
+        assertEquals("a", Util.invokeMethodAnyway(parent, "getA"));
+        assertThrows(NoSuchMethodException.class, () -> Util.invokeMethodAnyway(parent, "getC"));
+        assertEquals("a", Util.invokeMethodAnyway(child, "getA"));
+        assertEquals("c", Util.invokeMethodAnyway(child, "getC"));
+
+        assertEquals("b", Util.invokeMethodAnyway(parent, "getB"));
+        assertEquals("b", Util.invokeMethodAnyway(child, "getB"));
     }
 
-    class TestClass {
-        private String method() {
-            return "successMethod";
-        }
+    @Test
+    public void invokeStaticMethod() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        assertEquals("a", Util.invokeStaticMethod(Parent.class, "genA"));
+        assertThrows(NoSuchMethodException.class, () -> Util.invokeStaticMethod(Parent.class, "genC"));
+        assertEquals("a", Util.invokeStaticMethod(Child.class, "genA"));
+        assertEquals("c", Util.invokeStaticMethod(Child.class, "genC"));
 
-        private String methodManyParams(String a, String b) {
-            return "successMethod" + a + b;
-        }
+        assertThrows(NoSuchMethodException.class, () -> Util.invokeStaticMethod(Parent.class, "genB"));
+        assertThrows(NoSuchMethodException.class, () -> Util.invokeStaticMethod(Child.class, "genB"));
     }
 
-    @Retention(RetentionPolicy.RUNTIME)
-    @interface TestRuntimeAnnotation {
-        String method() default "sucessMethodOnAnnotation";
+    @Test
+    public void invokeStaticMethodAnyway() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        assertEquals("a", Util.invokeStaticMethodAnyway(Parent.class, "genA"));
+        assertThrows(NoSuchMethodException.class, () -> Util.invokeStaticMethodAnyway(Parent.class, "genC"));
+        assertEquals("a", Util.invokeStaticMethodAnyway(Child.class, "genA"));
+        assertEquals("c", Util.invokeStaticMethodAnyway(Child.class, "genC"));
+
+        assertEquals("b", Util.invokeStaticMethodAnyway(Parent.class, "genB"));
+        assertEquals("b", Util.invokeStaticMethodAnyway(Child.class, "genB"));
     }
 
-    @TestRuntimeAnnotation
-    class AnnotationOnClass {
-
+    @Test
+    public void getField() throws IllegalAccessException, NoSuchFieldException {
+        Parent parent = new Parent();
+        Child child = new Child();
+        assertEquals("a", Util.getField(parent, "a"));
+        assertEquals("a", Util.getField(child, "a"));
+        assertEquals("c", Util.getField(child, "c"));
+        assertThrows(NoSuchFieldException.class, () -> Util.getField(parent, "c"));
+        assertThrows(NoSuchFieldException.class, () -> Util.getField(parent, "b"));
+        assertThrows(NoSuchFieldException.class, () -> Util.getField(child, "b"));
     }
+
+    @Test
+    public void getFieldAnyway() throws IllegalAccessException, NoSuchFieldException {
+        Parent parent = new Parent();
+        Child child = new Child();
+        assertEquals("a", Util.getFieldAnyway(parent, "a"));
+        assertEquals("a", Util.getFieldAnyway(child, "a"));
+        assertEquals("c", Util.getFieldAnyway(child, "c"));
+        assertEquals("d", Util.getFieldAnyway(child, "d"));
+        assertEquals("b", Util.getFieldAnyway(child, "b"));
+        assertThrows(NoSuchFieldException.class, () -> assertEquals("c", Util.getFieldAnyway(parent, "c")));
+    }
+
+    @Test
+    public void setField() throws IllegalAccessException, NoSuchFieldException {
+        Parent parent = new Parent();
+        Child child = new Child();
+        Util.setField(parent, "a", "aa");
+        Util.setField(child, "a", "aa");
+        Util.setField(child, "c", "cc");
+        assertThrows(NoSuchFieldException.class, () -> Util.setField(parent, "c", "cc"));
+        assertThrows(NoSuchFieldException.class, () -> Util.setField(parent, "b", "bb"));
+        assertThrows(NoSuchFieldException.class, () -> Util.setField(child, "b", "bb"));
+
+        assertEquals("aa", Util.getField(parent, "a"));
+        assertEquals("aa", Util.getField(child, "a"));
+        assertEquals("cc", Util.getField(child, "c"));
+    }
+
+    @Test
+    public void setFieldAnyway() throws IllegalAccessException, NoSuchFieldException {
+        Parent parent = new Parent();
+        Child child = new Child();
+        Util.setFieldAnyway(parent, "a", "aa");
+        Util.setFieldAnyway(child, "a", "aa");
+        Util.setFieldAnyway(child, "c", "cc");
+        Util.setFieldAnyway(child, "d", "dd");
+        Util.setFieldAnyway(child, "b", "bb");
+        assertThrows(NoSuchFieldException.class, () -> Util.setFieldAnyway(parent, "c", "cc"));
+
+
+        assertEquals("aa", Util.getFieldAnyway(parent, "a"));
+        assertEquals("aa", Util.getFieldAnyway(child, "a"));
+        assertEquals("cc", Util.getFieldAnyway(child, "c"));
+        assertEquals("dd", Util.getFieldAnyway(child, "d"));
+        assertEquals("bb", Util.getFieldAnyway(child, "b"));
+    }
+}
+
+class Parent {
+    public String a = "a";
+    private String b = "b";
+
+    public String getA() {
+        return a;
+    }
+
+    private String getB() {
+        return b;
+    }
+
+    public static String genA() {
+        return "a";
+    }
+
+    private static String genB() {
+        return "b";
+    }
+}
+
+class Child extends Parent {
+    public String c = "c";
+    private String d = "d";
+
+    public String getC() {
+        return c;
+    }
+
+    private String getD() {
+        return d;
+    }
+
+    public static String genC() {
+        return "c";
+    }
+
+    public static String genD() {
+        return "d";
+    }
+}
+
+class TestClass {
+    private String method() {
+        return "successMethod";
+    }
+
+    private String methodManyParams(String a, String b) {
+        return "successMethod" + a + b;
+    }
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface TestRuntimeAnnotation {
+    String method() default "sucessMethodOnAnnotation";
+}
+
+@TestRuntimeAnnotation
+class AnnotationOnClass {
+
 }
