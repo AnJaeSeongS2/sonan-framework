f90ab551d1f7c69d349fcb766eafed0f424ac1dd
AnJaeSeongS2 <jindam91@naver.com>
2021-04-07 09:40:23 +0900

    [trivial][java-util] test code에서 ide 버그로 noti뜨는 것 제거.

--- woowahan-di-framework/src/test/java/com/woowahan/framework/json/JacksonUtilTest.java
+++ woowahan-di-framework/src/test/java/com/woowahan/framework/json/JacksonUtilTest.java
@@ -1,11 +1,7 @@
 package com.woowahan.framework.json;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.woowahan.framework.json.throwable.FailedConvertJsonException;
-import com.woowahan.framework.web.annotation.model.Id;
 import org.junit.jupiter.api.Test;
-
-import java.beans.ConstructorProperties;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -34,43 +30,4 @@ class JacksonUtilTest {
         assertEquals(obj, otherShopFromSameResource);
     }
 
-}
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-class TestClass {
-
-    @Id
-    private Integer id;
-    private String name;
-
-    @ConstructorProperties({"id", "name"})
-    public TestClass(Integer id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    @Override
-    public boolean equals(Object target) {
-        if (target == null)
-            return false;
-        if (target.getClass() != this.getClass() || !(target instanceof TestClass))
-            return false;
-        TestClass targetShop = (TestClass) target;
-        return this.id == targetShop.id && this.name.equals(targetShop.name);
-    }
 }
